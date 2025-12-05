5288a1ba81cd1d6d7fead8dfea12bf991d97a50a
Aleksandr Vishniakov <asvishnyakov@hotmail.com>
2025-12-05 11:54:14 +0200

    fix: use only https for platform

--- VirtoCommerce.Platform.script.sh
+++ VirtoCommerce.Platform.script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export ASPNETCORE_ENVIRONMENT=Development
-dotnet VirtoCommerce.Platform.Web.dll --urls="http://localhost:5000;http://192.168.176.84:5000;https://localhost:5001;https://192.168.176.84:5001" | lolcat
+dotnet VirtoCommerce.Platform.Web.dll --urls="https://localhost:5001" | lolcat
 exec bash
